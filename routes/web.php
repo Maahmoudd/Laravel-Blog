04b018414eb6bb457f34e6cb3eaab86073cb049f
Mahmoud EL-Sabahy <73112243+Maahmoudd@users.noreply.github.com>
2024-01-10 01:01:34 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,26 @@
-<?php
-
-use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\AdminPostController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\AdminPostController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store'])->name('comments.store');
 
-Route::get('posts/{post:slug}',[PostController::class, 'show']);
-Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+Route::post('newsletter', [NewsletterController::class, 'store'])->name('newsletter.store');
 
-Route::post('newsletter', NewsletterController::class);
-
-Route::get('register', [RegisterController::class ,'create'])->middleware('guest');
-Route::post('register', [RegisterController::class ,'store'])->middleware('guest');
-
-Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
-Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
-
-Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
-
-//Admin
-
-Route::middleware('can:admin')->group(function (){
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
-    Route::get('admin/posts', [AdminPostController::class, 'index']);
-    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
-    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
-
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])->name('register.create');
+    Route::post('register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('login', [SessionsController::class, 'create'])->name('login.create');
+    Route::post('login', [SessionsController::class, 'store'])->name('login.store');
 });
 
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth')->name('logout');
+
+Route::middleware(['can:admin', 'prefix' => 'admin'])->group(function () {
+    Route::resource('posts', AdminPostController::class);
+});
